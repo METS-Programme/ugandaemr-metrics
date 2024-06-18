d82ee53f7f538a8aae53912528f92c232d102530
Isaac Akileng <isaacakileng56@gmail.com>
2024-06-18 17:17:21 +0300

    Fix NaN on tiles using new api

--- src/components/ugemr-metrics/Metrics.js
+++ src/components/ugemr-metrics/Metrics.js
@@ -4,7 +4,7 @@ import { CheckmarkOutline, Store, DevicesApps, GroupPresentation, UserMultiple }
 import "@carbon/charts/styles.css";
 import {DataTableComponent} from "../data-table.component";
 import ViewButton from "../view-button";
-import {fourXheaders, rows, threeXHeaders} from "../../constants";
+import {fourXheaders, threeXHeaders} from "../../constants";
 import dayjs from "dayjs";
 
 const Metrics = (props) => {
@@ -12,9 +12,12 @@ const Metrics = (props) => {
   const recordsCaptured = () => {
     let count = 0;
     metricsData?.forEach((record) => {
-        record?.value?.dataentry?.forEach((item) => {
-            count += item?.numberOfEntries;
-        });
+      const triage = record?.value?.dataentry[0]?.['Triage'] ?? 0;
+      const clinician = record?.value?.dataentry[0]?.['Clinician'] ?? 0;
+      const lab = record?.value?.dataentry[0]?.['Lab'] ?? 0;
+      const pharmacy = record?.value?.dataentry[0]?.['Pharmacy'] ?? 0;
+
+      count += (triage + clinician + lab + pharmacy);
     });
 
     return count;
@@ -49,18 +52,13 @@ const Metrics = (props) => {
     const facility = [];
     metricsData?.forEach((record, index) => {
       if (record?.emrversion === "4.0.0-SNAPSHOT") {
-        let count = 0;
-        record?.value?.dataentry?.forEach((item) => {
-          count += item?.numberOfEntries;
-        });
-
         facility.push({
           id: `${index++}`,
           facility: record?.facilityname,
-          triage: 100,
-          clinician: 50,
-          lab: 0,
-          pharmacy: 0,
+          triage: record?.value?.dataentry[0]?.['Triage'] ?? 0,
+          clinician: record?.value?.dataentry[0]?.['Clinician'] ?? 0,
+          lab: record?.value?.dataentry[0]?.['Lab'] ?? 0,
+          pharmacy: record?.value?.dataentry[0]?.['Pharmacy'] ?? 0,
 
         })
       }
@@ -69,88 +67,112 @@ const Metrics = (props) => {
     return facility;
   }
     return (
-        <>
-          <div className="tile-container">
-            <div className="tile tile-margin">
-              <div className="tile-header">
-                <div className="tile-items-container">
-                  <div className="tile-icon"><DevicesApps size={50}/></div>
-                  <div> UgandaEMR</div>
-                </div>
-              </div>
-              <div className="emr-details-table">
-                <table>
-                  <tbody>
-                  <tr>
-                    <td>Version:</td>
-                    <td className="emr-version"> {'4.0.0-SNAPSHOT'} <CheckmarkOutline size={15}/></td>
-                  </tr>
-                  <tr>
-                    <td>Tools:</td>
-                    <td> 8 </td>
-                  </tr>
-                  </tbody>
-                </table>
+      <>
+        <div className="tile-container">
+          <div className="tile tile-margin">
+            <div className="tile-header">
+              <div className="tile-items-container">
+                <div className="tile-icon"><DevicesApps size={50}/></div>
+                <div> UgandaEMR</div>
               </div>
             </div>
-            <div className="tile tile-margin">
-              <div className="tile-header">
-                <div className="tile-items-container">
-                  <div className="tile-icon"><Store size={50}/></div>
-                  <div> Health Facilities</div>
-                </div>
-                <div className="tile-bottom-style">
-                  <div className="tile-item-value"> {metricsData?.length + rows.length }</div>
-                  <ViewButton/>
-                </div>
-              </div>
+            <div className="emr-details-table">
+              <table>
+                <tbody>
+                <tr>
+                  <td>Version:</td>
+                  <td className="emr-version"> {'4.0.0-SNAPSHOT'}
+                    <CheckmarkOutline size={15}/></td>
+                </tr>
+                <tr>
+                  <td>Tools:</td>
+                  <td> 8</td>
+                </tr>
+                </tbody>
+              </table>
             </div>
-            <div className="tile tile-margin">
-              <div className="tile-header">
-                <div className="tile-items-container">
-                  <div className="tile-icon"><GroupPresentation size={50}/>
-                  </div>
-                  <div>Patient Served</div>
-                </div>
-                <div className="tile-bottom-style">
-                  <div className="tile-item-value"> {recordsCaptured()} </div>
-                  <ViewButton/>
-                </div>
+          </div>
+          <div className="tile tile-margin">
+            <div className="tile-header">
+              <div className="tile-items-container">
+                <div className="tile-icon"><Store size={50}/></div>
+                <div> Health Facilities</div>
               </div>
-            </div>
-            <div className="tile">
-              <div className="tile-header">
-                <div className="tile-items-container">
-                  <div className="tile-icon"><UserMultiple size={50}/></div>
-                  <div> Data Entry Statistics</div>
-                </div>
-                <div className="tile-bottom-style">
-                  <div className="tile-item-value"> {recordsCaptured() + facilityDetails().dataEntryCount} </div>
-                  <ViewButton/>
-                </div>
+              <div className="tile-bottom-style">
+                <div className="tile-item-value"> {metricsData?.length}</div>
+                <ViewButton/>
               </div>
             </div>
           </div>
+          <div className="tile tile-margin">
+            <div className="tile-header">
+              <div className="tile-items-container">
+                <div className="tile-icon"><GroupPresentation size={50}/>
+                </div>
+                <div>Patient Served</div>
+              </div>
+              <div className="tile-bottom-style">
+                <div className="tile-item-value"> {recordsCaptured()} </div>
+                <ViewButton/>
+              </div>
+            </div>
+          </div>
+          <div className="tile">
+            <div className="tile-header">
+              <div className="tile-items-container">
+                <div className="tile-icon"><UserMultiple size={50}/></div>
+                <div> Data Entry Statistics</div>
+              </div>
+              <div className="tile-bottom-style">
+                <div
+                  className="tile-item-value"> {recordsCaptured() + facilityDetails().dataEntryCount} </div>
+                <ViewButton/>
+              </div>
+            </div>
+          </div>
+        </div>
 
-          <div className="item-chart-container">
-            <div className="item-chart item-chart-left">
-              <div className="cds--cc--title">
-                <p className="title" role="heading" aria-level="2">
-                  UgandaEMR+ ({dayjs(dates[0]).format("DD/MMM/YYYY")} - {dayjs(dates[1]).format("DD/MMM/YYYY")})
-                </p>
-              </div>
-              <DataTableComponent rows={facilityDetailsPlus()} headers={fourXheaders} />
+        <div className="item-chart-container">
+          <div className="item-chart item-chart-left">
+            <div className="cds--cc--title">
+              <p className="title" role="heading" aria-level="2">
+                UgandaEMR+
+                ({dayjs(dates[0]).format("DD/MMM/YYYY")} - {dayjs(dates[1]).format("DD/MMM/YYYY")})
+              </p>
             </div>
-            <div className="item-chart">
-              <div className="cds--cc--title">
-                <p className="title" role="heading" aria-level="2">
-                  UgandaEMR 3.x ({dayjs(dates[0]).format("DD/MMM/YYYY")} - {dayjs(dates[1]).format("DD/MMM/YYYY")})
-                </p>
-              </div>
-              <DataTableComponent rows={facilityDetails().facility} headers={threeXHeaders}/>
-            </div>
+            <DataTableComponent rows={facilityDetailsPlus()}
+                                headers={fourXheaders}/>
           </div>
-        </>
+          <div className="item-chart">
+            <div className="cds--cc--title">
+              <p className="title" role="heading" aria-level="2">
+                UgandaEMR+ Coverage
+              </p>
+            </div>
+
+          </div>
+        </div>
+        <div className="item-chart-container">
+          <div className="item-chart item-chart-left">
+            <div className="cds--cc--title">
+              <p className="title" role="heading" aria-level="2">
+                UgandaEMR 3.x
+                ({dayjs(dates[0]).format("DD/MMM/YYYY")} - {dayjs(dates[1]).format("DD/MMM/YYYY")})
+              </p>
+            </div>
+            <DataTableComponent rows={facilityDetails().facility}
+                                headers={threeXHeaders}/>
+          </div>
+          <div className="item-chart">
+            <div className="cds--cc--title">
+              <p className="title" role="heading" aria-level="2">
+                UgandaEMR 3.x
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </>
     );
 };
 
