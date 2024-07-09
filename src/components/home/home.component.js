507c5f805807a2c7eea2a5161d1186984e855b5b
Isaac Akileng <isaacakileng56@gmail.com>
2024-07-09 09:25:53 +0300

    Add EMR switcher on menu

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -2,26 +2,46 @@ import React, { useState } from 'react';
 import {
   Content,
   SideNav,
-  SideNavItems, SideNavLink,
+  SideNavItems,
+  SideNavLink,
+  OverflowMenu,
+  OverflowMenuItem,
 } from "@carbon/react";
 import "@carbon/charts/styles.css";
 import './home.css';
-import {DataShare, Microscope, ShareKnowledge, Tour} from "@carbon/icons-react";
+import {DataShare, Microscope, ShareKnowledge, Tour, Screen} from "@carbon/icons-react";
 import CoverageComponent from "../nav-Items/coverage";
 import PocComponent from "../nav-Items/poc";
 
 const HomeComponent = () => {
-  const [switchName, setSwitchName] = useState("eafya");
+  const [switchName, setSwitchName] = useState("ugandaemr");
   const [navItem, setNavItem] = useState("coverage");
 
   const handleOnClickItem = (navItem) => {
     setNavItem(navItem);
   };
 
+  const handleSwitchName = (selectedItem) => {
+    setSwitchName(selectedItem);
+  }
+
   return (
     <>
       <header className="metrics-header">
         <div className="header-label"> EMR Metrics</div>
+        <div>
+          <label> {switchName === "ugandaemr" ? "UgandaEMR" : switchName === "eafya" ? "eAFYA" : "Clinic Master"} </label>
+          <OverflowMenu
+            renderIcon={Screen}
+            flipped={true}
+            iconDescription="EMR"
+            className="emr-menu-container"
+          >
+            <OverflowMenuItem itemText="UgandaEMR" onClick={() => handleSwitchName("ugandaemr")} />
+            <OverflowMenuItem hasDivider itemText="eAFYA" onClick={() => handleSwitchName("eafya")} />
+            <OverflowMenuItem hasDivider itemText="Clinic Master" onClick={() => handleSwitchName("clinicmaster")} />
+          </OverflowMenu>
+        </div>
       </header>
       <Content className="metrics-body">
         <SideNav aria-label="Side navigation">
